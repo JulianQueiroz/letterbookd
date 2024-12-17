8a1e40af85a74fc5fdb4384b8032ef6d8645db96
Juliana <julianatko@gmail.com>
2024-12-17 20:46:30 -0300

    📦 NEW: rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Book from './pages/Book';
 
 function App() {
 
   return (
-    <>
-    <Home/>
-    </>
+    <Router>
+        <Routes>
+            <Route path='/' element= {<Home/>} />
+            <Route path='/book/:id' element= {<Book/>} />
+        </Routes>
+    </Router>
   )
 }
 
 export default App
+
