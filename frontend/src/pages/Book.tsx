82c61c79fc023fe7ae3d803a7345904bca105b2f
Juliana <julianatko@gmail.com>
2024-12-17 22:18:07 -0300

    📦 NEW: implementaçao do rating

--- frontend/src/pages/Book.tsx
+++ frontend/src/pages/Book.tsx
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchBookDetails } from "../api/api";
+import ModalReview from "../components/ModalReview";
+import { Rating } from "@mui/material";
 
 export default function Book() {
   const { id } = useParams();
@@ -43,6 +45,15 @@ export default function Book() {
 
   return (
     <div className="container">
+      <div className="d-flex justify-start">
+        <button
+          type="button"
+          className="border-0 bg-transparent"
+          onClick={() => window.history.back()}
+        >
+          <i className="fa-solid fa-arrow-left"></i>
+        </button>
+      </div>
       <div className="card mt-3 align-items-center d-flex justify-content-center">
         <div className="card-title">{data.title}</div>
         {/* <div className="card-body"><img src={data.imageLinks.thumbnail} alt=""/></div> */}
@@ -50,12 +61,69 @@ export default function Book() {
         <div className="card-body">{data.description}</div>
         <div className="card-body">{data.authors}</div>
         <div className="d-flex flex-row">
-            <i className="fa-regular fa-star fa-2x"></i>
-            <i className="fa-regular fa-star fa-2x"></i>
-            <i className="fa-regular fa-star fa-2x"></i>
-            <i className="fa-solid fa-star fa-2x"></i>
-            <i className="fa-regular fa-star fa-2x"></i>
+            <Rating/>
         </div>
+        <div className="d-flex w-100 justify-content-end">
+          <button
+            type="button"
+            className="btn bg-transparent"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <i className="fa-solid fa-plus fa-2x"></i>
+          </button>
+        </div>
+      </div>
+      <ModalReview/>
+      <hr className="w-100" />
+      <h2 className="text-start">Avaliações:</h2>
+      <div className="d-flex flex-row">
+        <div style={{ marginRight: "1em" }}>
+          <i className="fa-solid fa-user"></i>
+        </div>
+        <p className="text-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          necessitatibus minus, tenetur, quae ducimus, ipsa fuga recusandae
+          molestiae autem totam dicta possimus voluptas nihil eum laboriosam
+          doloribus corrupti sed officiis!
+        </p>
+        <i className="fa-regular fa-heart"></i>
+      </div>
+      <div className="d-flex flex-row">
+        <div style={{ marginRight: "1em" }}>
+          <i className="fa-solid fa-user"></i>
+        </div>
+        <p className="text-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          necessitatibus minus, tenetur, quae ducimus, ipsa fuga recusandae
+          molestiae autem totam dicta possimus voluptas nihil eum laboriosam
+          doloribus corrupti sed officiis!
+        </p>
+        <i className="fa-regular fa-heart"></i>
+      </div>
+      <div className="d-flex flex-row">
+        <div style={{ marginRight: "1em" }}>
+          <i className="fa-solid fa-user"></i>
+        </div>
+        <p className="text-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          necessitatibus minus, tenetur, quae ducimus, ipsa fuga recusandae
+          molestiae autem totam dicta possimus voluptas nihil eum laboriosam
+          doloribus corrupti sed officiis!
+        </p>
+        <i className="fa-regular fa-heart"></i>
+      </div>
+      <div className="d-flex flex-row">
+        <div style={{ marginRight: "1em" }}>
+          <i className="fa-solid fa-user"></i>
+        </div>
+        <p className="text-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          necessitatibus minus, tenetur, quae ducimus, ipsa fuga recusandae
+          molestiae autem totam dicta possimus voluptas nihil eum laboriosam
+          doloribus corrupti sed officiis!
+        </p>
+        <i className="fa-regular fa-heart"></i>
       </div>
     </div>
   );
